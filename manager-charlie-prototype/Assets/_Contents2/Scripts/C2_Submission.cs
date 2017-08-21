9ce09b6903b38c0d7f987dca2faf91ab2eff91d9
DESKTOP-0NDPIE0\GGulBBang <ryoungtae22@gmail.com>
2017-08-21 17:33:24 +0900

    문제 출제 기능 구현 중

---
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/C2_Submission.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class C2_Submission : MonoBehaviour {
+
+    public Transform BackGround;
+
+	void Start ()
+    {
+        StartCoroutine("ShowSubmission");
+	}
+
+    IEnumerator ShowSubmission()
+    {
+        // 애니메이션 리소스가 오면 Debug를 빼고 애니메이션을 삽입할 자리
+        Debug.Log("Animation Start");
+
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("Animation End");
+
+        yield return new WaitForSeconds(1.0f);
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(BackGround.DOMoveY(400, 1));
+    }
+}
