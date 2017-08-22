6a20c07558103015d0183b04ef1e5198f7d9b9ef
Youngtae <ryoungtae22@gmail.com>
2017-08-22 11:04:48 +0900

    문제 제출 기능 구현 중

--- manager-charlie-prototype/Assets/_Contents2/Scripts/Submission.cs
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/Submission.cs
@@ -12,6 +12,14 @@ namespace Content2
         public Transform BackGround;        // 백 그라운드를 움직일 수 있게 해주는 대상
         public Transform Question1;         // 문제 1의 자리
         public Transform Question2;         // 문제 2의 자리
+        public GameObject Red;               // 문제 1번 프리팹
+        public GameObject Blue;              // 문제 2번 프리팹
+
+        void Awake()
+        {
+            Instantiate(Red, Question1);        // 문제 생성 랜덤으로 바꾼다.
+            Instantiate(Blue, Question2);       
+        }
 
         void Start()
         {
