2c173fae830f0e1a41458cf46c4605ecd98fb022
Youngtae <ryoungtae22@gmail.com>
2017-08-22 10:35:11 +0900

    스크립트 이름 변경

---
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/Qexposure.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+namespace Content2
+{
+    public class Qexposure : MonoBehaviour
+    {
+        public Transform Target;                    // 문제 제출 대상
+        public GameObject NamePanel;                // 문제 제출 대상의 이름 Text의 부모 GameObject
+        public Text NameText;                       // 문제 제출 대상의  이름 Text
+        public GameObject ButtonPanel;              // 문제 제출이 다되었을 경우 클릭을 할 수 있게 해주는 버튼의 부모  GameObject
+
+        void Start()
+        {
+            StartCoroutine("SeqShowName");
+        }
+
+        IEnumerator SeqShowName()
+        {
+            Vector3 XY;
+            XY.x = 2;
+            XY.y = 2;
+            XY.z = 1;
+
+            NamePanel.SetActive(false);
+
+            // 이미지 리소스가 도착하면 이미지 리소스로 교체..해야하는데..
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Append(Target.DOScale(XY, 1));
+            mySequence.Play();
+
+            yield return new WaitForSeconds(1.5f);
+            NamePanel.SetActive(true);
+            // 기획서가 도착하면 NameText를 문제로 바꿔줘야할 자리
+
+            yield return new WaitForSeconds(1.5f);
+            Debug.Log("Next Scene Set Ready");
+            ButtonPanel.SetActive(true);
+        }
+
+        public void Buttonclick()
+        {
+            SceneManager.LoadScene("C2_Submission");
+        }
+    }
+}
---
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/Submission.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace Content2
+{
+    public class Submission : MonoBehaviour
+    {
+
+        public Transform BackGround;        // 백 그라운드를 움직일 수 있게 해주는 대상
+        public Transform Question1;         // 문제 1의 자리
+        public Transform Question2;         // 문제 2의 자리
+
+        void Start()
+        {
+            StartCoroutine("SeqShowSubmission");
+        }
+
+        IEnumerator SeqShowSubmission()
+        {
+            // 애니메이션 리소스가 오면 Debug를 빼고 애니메이션을 삽입할 자리
+            Debug.Log("Animation Start");
+
+            yield return new WaitForSeconds(1.0f);
+            Debug.Log("Animation End");
+
+            yield return new WaitForSeconds(1.0f);
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Append(BackGround.DOMoveY(400, 1));
+            mySequence.Play();
+        }
+    }
+}
