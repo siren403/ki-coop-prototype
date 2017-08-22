d4f454657ef2d0ac5ecfe622fe9ba87878ff728c
MASTER\Administrator <sarangsiteru>
2017-08-22 11:04:23 +0900

    정리 (커밋 병합용)

--- manager-charlie-prototype/Assets/_Contents3/Scripts/UICanvasBtn.cs
+++ manager-charlie-prototype/Assets/_Contents3/Scripts/UICanvasBtn.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 // 버튼 기능 클래스
 public class UICanvasBtn : MonoBehaviour {
 
-	
+
+
+    public void OnClickTestBtn1()
+    {
+        
+    }
+    public void OnClickTestBtn2()
+    {
+
+    }
 
 
 
