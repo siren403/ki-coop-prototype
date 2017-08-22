fbc1bcb168ce45c5639a7a0a06a5f3622d250c59
Youngtae <ryoungtae22@gmail.com>
2017-08-22 17:03:42 +0900

    문제 랜덤 출제 기능 완료

--- manager-charlie-prototype/Assets/_Contents2/Scripts/Qexposure.cs
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/Qexposure.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using CustomDebug;
 
 namespace Content2
 {
     public class Qexposure : MonoBehaviour
     {
+        
         public Transform Target;                    // 문제 제출 대상
         public GameObject NamePanel;                // 문제 제출 대상의 이름 Text의 부모 GameObject
         public Text NameText;                       // 문제 제출 대상의  이름 Text
-        public GameObject ButtonPanel;              // 문제 제출이 다되었을 경우 클릭을 할 수 있게 해주는 버튼의 부모  GameObject
+        public GameObject ButtonPanel;              // 문제 제출 Animation이 완료 되었을 경우 클릭을 할 수 있게 해주는 버튼의 부모 GameObject
 
         void Start()
         {
@@ -21,30 +23,30 @@ namespace Content2
 
         IEnumerator SeqShowName()
         {
-            Vector3 XY;
-            XY.x = 2;
-            XY.y = 2;
-            XY.z = 1;
+            Vector3 Xy;     // 문제 제출 대상의 Size 확대 Vector 값 설정
+            Xy.x = 2;
+            Xy.y = 2;
+            Xy.z = 1;
 
             NamePanel.SetActive(false);
 
             // 이미지 리소스가 도착하면 이미지 리소스로 교체..해야하는데..
-            Sequence mySequence = DOTween.Sequence();
-            mySequence.Append(Target.DOScale(XY, 1));
-            mySequence.Play();
+            Sequence MySequence = DOTween.Sequence();
+            MySequence.Append(Target.DOScale(Xy, 1));
+            MySequence.Play();
 
             yield return new WaitForSeconds(1.5f);
             NamePanel.SetActive(true);
             // 기획서가 도착하면 NameText를 문제로 바꿔줘야할 자리
 
             yield return new WaitForSeconds(1.5f);
-            Debug.Log("Next Scene Set Ready");
+            CDebug.Log("Next Scene Set Ready");
             ButtonPanel.SetActive(true);
         }
 
-        public void Buttonclick()
+        public void SceneSubmissionBtn()
         {
-            SceneManager.LoadScene("C2_Submission");
+            SceneManager.LoadScene("SceneSubmission");
         }
     }
 }
--- manager-charlie-prototype/Assets/_Contents2/Scripts/Submission.cs
+++ manager-charlie-prototype/Assets/_Contents2/Scripts/Submission.cs
@@ -3,40 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using CustomDebug;
 
 namespace Content2
 {
     public class Submission : MonoBehaviour
     {
 
-        public Transform BackGround;        // 백 그라운드를 움직일 수 있게 해주는 대상
-        public Transform Question1;         // 문제 1의 자리
-        public Transform Question2;         // 문제 2의 자리
-        public GameObject Red;               // 문제 1번 프리팹
-        public GameObject Blue;              // 문제 2번 프리팹
+        public Transform backGround;        // 백 그라운드를 움직일 수 있게 해주는 대상
+        public GameObject question;         // 문제 대상
+        public Transform question1;         // 문제 1의 자리
+        public Transform question2;         // 문제 2의 자리
+        public GameObject answer;           // 정답 프리팹, Transform으로 추후에 변경
+        public GameObject wAnswer;          // 오답 프리팹, Transform으로 추후에 변경
 
-        void Awake()
-        {
-            Instantiate(Red, Question1);        // 문제 생성 랜덤으로 바꾼다.
-            Instantiate(Blue, Question2);       
-        }
 
         void Start()
         {
+            QuestionData();                               // QustionData -> QustionSubmit -> StartCoroutine("SeqShowSubmission")
+        }
+
+        void QuestionData()
+        {
+            // question,answer,wAnswer의 데이터를 바꿔준다.
+            CDebug.Log("Data Set Complete.");
+            QuestionSubmit();
+        }
+
+        void QuestionSubmit()
+        {
+            int randomSubmit = Random.Range(0, 2);      // 문제의 답을 랜덤으로 섞어준다.
+
+            if (randomSubmit == 0)
+            {
+                // 나중에 문제에 대한 데이터가 나오면 Transform으로 위치 값만 바꿔준다.
+                Instantiate(answer, question1);
+                Instantiate(wAnswer, question2);
+            }
+            else
+            {
+                Instantiate(answer, question2);
+                Instantiate(wAnswer, question1);
+            }
+
             StartCoroutine("SeqShowSubmission");
         }
 
         IEnumerator SeqShowSubmission()
         {
             // 애니메이션 리소스가 오면 Debug를 빼고 애니메이션을 삽입할 자리
-            Debug.Log("Animation Start");
+            CDebug.Log("Animation Start");
 
             yield return new WaitForSeconds(1.0f);
-            Debug.Log("Animation End");
+            CDebug.Log("Animation End");
 
             yield return new WaitForSeconds(1.0f);
             Sequence mySequence = DOTween.Sequence();
-            mySequence.Append(BackGround.DOMoveY(400, 1));
+            mySequence.Append(backGround.DOMoveY(400, 1));
             mySequence.Play();
         }
     }
