83f02a91a00d00a431a4ef197c8f85166c07b55c
Byeongyup <kby7139@gmail.com>
2017-08-23 11:06:30 +0900

    임시 커밋

--- manager-charlie-prototype/Assets/_Contents1/Scripts/InstContents1Manager.cs
+++ manager-charlie-prototype/Assets/_Contents1/Scripts/InstContents1Manager.cs
@@ -57,7 +57,7 @@ public class InstContents1Manager : MonoBehaviour {
 
         mCon1EpisodeLoaction = 1;           // 추후 값 수정 필요
         mCorrectAnswerCount = 0;
-        mCon1QuestionCount = 1;
+        mCon1QuestionCount = 0;
     }
 	
 	// Update is called once per frame
@@ -87,44 +87,137 @@ public class InstContents1Manager : MonoBehaviour {
         Content1inCorrectSetting();
     }
 
-    /** 정답 셋팅 */
+    /** 정답 셋팅 */ //-> 추후에 로직 개선 필요, 마구잡이식으로 짠 코드라 상당히 지저분하고 알아보기가 힘이든다.
     void Content1CorrectSetting()
     {
-        // 임시 지역 변수
-        string tmpString = null;
-        string[] tmpStrings = new string[3]; 
-        int tmpStringCount = 0;
+        // 임시 지역 변수들
+        string[] tmpUsableWords = new string[10];   // 사용한 단어들을 저장하는 문자 배열
+        int tmpStringCount = 0;                     // 카운트 변수
 
-        // 콘텐츠 위치에 따라서
+        // 콘텐츠의 어느 에피소드를 진행하고 있는지에 따라 기본 정보들을 셋팅하는 로직
         if(mCon1EpisodeLoaction == 1)
         {
-            // 문제 개수 체크
+            // 에피소드 1에 등장하는 단어들을 저장하는 임시 배열들.
+            // 여기에서만 사용되기 때문에 여기에 선언하여 사용. -> 이들을 정리할 수 있는 클래스나 기능이 추가될 경우 로직 수정
+            string[] tmpSaveTextA = new string[3];
+            string[] tmpSaveTextB = new string[3];
+            string[] tmpSaveTextC = new string[3];
+            string[] tmpSaveTextD = new string[3];
+            string[] tmpSaveTextE = new string[3];
+
+            int[] tmpSettingArr = new int[4];
+
+            // string 배열 5개에 이번 에피소드에 등장하는 파닉스 음과 관련한 단어들을 저장하는 로직
+            // 첫번째 for문-------------
+            for (int i=0; i<5; i++)
+            {
+                tmpStringCount = 0;
+
+                // 중첩, 2번째 for문-----------------
+                for (int j = 0; j < mWordsData.Words.Count; j++)
+                {
+                    // i가 0일 경우 'A'와 관련된 단어들을 tmpSaveTextA 배열에 저장
+                    if(i == 0)
+                    {
+                        // 모든 리스트를 검색하기 때문에 일정 요건만 확인하고 벗어남.
+                        if (tmpStringCount >= 3)
+                            break;
+
+                        if(mWordsData.Words[j].Word.Substring(0, 1) == "A")
+                        {
+                            tmpSaveTextA[tmpStringCount] = mWordsData.Words[j].Word;
+                        }
+                    }
+                    // "B"
+                    else if(i == 1)
+                    {
+                        if (tmpStringCount >= 3)
+                            break;
+
+                        if (mWordsData.Words[j].Word.Substring(0, 1) == "B")
+                        {
+                            tmpSaveTextA[tmpStringCount] = mWordsData.Words[j].Word;
+                        }
+                    }
+                    // "C"
+                    else if (i == 2)
+                    {
+                        if (tmpStringCount >= 3)
+                            break;
+
+                        if (mWordsData.Words[j].Word.Substring(0, 1) == "C")
+                        {
+                            tmpSaveTextA[tmpStringCount] = mWordsData.Words[j].Word;
+                        }
+                    }
+                    // "D"
+                    else if (i == 3)
+                    {
+                        if (tmpStringCount >= 3)
+                            break;
+
+                        if (mWordsData.Words[j].Word.Substring(0, 1) == "D")
+                        {
+                            tmpSaveTextA[tmpStringCount] = mWordsData.Words[j].Word;
+                        }
+                    }
+                    // "E"
+                    else if (i == 4)
+                    {
+                        if (tmpStringCount >= 3)
+                            break;
+
+                        if (mWordsData.Words[j].Word.Substring(0, 1) == "E")
+                        {
+                            tmpSaveTextA[tmpStringCount] = mWordsData.Words[j].Word;
+                        }
+                    }
+                } // 중첩 for문 종료
+            } // for문 최종 종료
+
+            // 출제할 정답과 오답 셋팅
             switch(mCon1QuestionCount)
             {
+                /* "A" */
                 case 0:
                 case 5:
-                    CDebug.Log("A");
-                    tmpString = "A";
+                    // 정답
+                    int tmpNumber = 0;                          // 이 지역에서 쓰이는 임시 랜덤 숫자 저장 변수
+                    int[] tmpUsePhanix = new int[3];            // 사용한 파닉스 알파벳을 저장하는 임시 배열
+                    string[] tmpUseWord = new string[3];        // 사용한 단어를 저장하는 임시 배열
+
+                    tmpNumber = Random.Range(0, 2);
+
+                    mCorrectAnswer = tmpSaveTextA[tmpNumber];
+
+                    // 오답
+                    for(int i=0; i<3; i++)
+                    {
+                        while(true)
+                        {
+                            tmpNumber = Random.Range(0, 3);
+
+                            
+
+                            break;
+                        }
+
+                        
+                    }
+
                     break;
+                // "B"
                 case 1:
                 case 6:
-                    CDebug.Log("B");
-                    tmpString = "B";
                     break;
                 case 2:
                 case 7:
-                    CDebug.Log("C");
-                    tmpString = "C";
                     break;
                 case 3:
                 case 8:
-                    CDebug.Log("D");
-                    tmpString = "D";
                     break;
                 case 4:
                 case 9:
-                    CDebug.Log("E");
-                    tmpString = "E";
                     break;
             }
         }
@@ -144,24 +237,6 @@ public class InstContents1Manager : MonoBehaviour {
         {
 
         }
-
-        CDebug.Log(tmpString);
-
-        for (int i=0; i<mWordsData.Words.Count-1; i++)
-        {
-            if (mWordsData.Words[i].Word.Substring(0, 1) == tmpString)
-            {
-
-                tmpStrings[tmpStringCount] = mWordsData.Words[i].Word;
-                tmpStringCount++;
-            }
-        }
-
-        int tmpNumber = Random.Range(0, 3);
-
-        mCorrectAnswer = tmpStrings[tmpNumber];
-
-        CDebug.Log(mCorrectAnswer);
     }
 
     /** 오답 셋팅 */
