2c7c2556744a4948a78dcfa39523563588603f86
5KIGS-17\John <jmonkeypig@gmail.com>
2017-08-23 15:45:29 +0900

    Episode 선택화면 Lock 완료

---
+++ manager-charlie-prototype/Assets/LoadingImgManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CustomDebug;
+using DG.Tweening;
+
+
+public class LoadingImgManager : MonoBehaviour {
+
+    public Image ImgLoading;
+
+    
+	// Use this for initialization
+	void Start () {
+        ImgLoading.GetComponent<Image>().DOFade(0,1);
+        StartCoroutine(DoActiveFalse());
+	}
+
+    IEnumerator DoActiveFalse()
+    {
+        yield return new WaitForSeconds(1.0f);
+        ImgLoading.gameObject.SetActive(false);
+    }
+
+}
