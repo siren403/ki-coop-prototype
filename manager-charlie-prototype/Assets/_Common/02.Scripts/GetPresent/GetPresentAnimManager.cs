bfbd776580fcd6b89082337a32bcb68c4e0c0b2e
5KIGS-17\John <jmonkeypig@gmail.com>
2017-08-23 16:59:27 +0900

    부모에게서 받은 Item 작업시작

---
+++ manager-charlie-prototype/Assets/_Common/02.Scripts/GetPresent/GetPresentAnimManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CustomDebug;
+using DG.Tweening;
+
+public class GetPresentAnimManager : MonoBehaviour {
+
+
+    public Transform ImgPresentBox;
+
+
+
+    public void AnimStart()
+    {
+        //* 선물 Box 연출 (임의설정) -> 추후 변경*/
+        ImgPresentBox.DORotate(Vector3.one * 10, 0.3f).SetLoops(-1, LoopType.Yoyo);
+        ImgPresentBox.DOScale(Vector3.one * 1.5f, 0.2f).SetLoops(-1, LoopType.Yoyo);
+    }
+}
---
+++ manager-charlie-prototype/Assets/_Common/02.Scripts/GetPresent/GetPresentManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CustomDebug;
+using DG.Tweening;
+
+
+public class GetPresentManager : MonoBehaviour {
+
+
+    public GetPresentAnimManager GetPresentAnimManagerObj;
+
+    // Use this for initialization
+    void Start () {
+
+        //* GetPresentAnimManager 의 AnimStart 를 호출해준다. */
+        GetPresentAnimManagerObj = GetPresentAnimManagerObj.GetComponent<GetPresentAnimManager>();
+        GetPresentAnimManagerObj.AnimStart();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
