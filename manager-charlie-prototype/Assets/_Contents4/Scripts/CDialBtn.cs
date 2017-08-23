87f24b7c98a8b3f870d5a1eb727150247d5c5544
hanwooyoung <hanwoozz91@gmail.com>
2017-08-23 19:20:57 +0900

    다이얼 스와이프 터치 입력 제어 구현 중

---
+++ manager-charlie-prototype/Assets/_Contents4/Scripts/CDialBtn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomDebug;
+
+
+public class CDialBtn : MonoBehaviour {
+    
+     
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+
+        //Click();
+	}
+
+    public void Click()
+    {
+        if(Input.GetMouseButtonDown(0) == true)
+        {
+            CDebug.Log("asd");
+        }
+    }
+
+
+}
