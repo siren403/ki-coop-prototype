3db88b6ff515c7672fe8f77556e8d9c38166d0e7
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-08-27 13:12:19 +0900

    - SimpleIO 구현 중

---
+++ manager-charlie-prototype/Assets/Examples/Util/ExamSimpleIO.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+using Util.Inspector;
+using CustomDebug;
+
+namespace Examples
+{
+    public class ExamSimpleIO : MonoBehaviour
+    {
+        private SimpleIO mIO = null;
+        private SimpleIO IO
+        {
+            get
+            {
+                if(mIO == null)
+                {
+                    mIO = new SimpleIO();
+                }
+                return mIO;
+            }
+        }
+
+        [Button]
+        public void Read()
+        {
+            string[] data = IO.ReadJson<string[]>("data/contents1.json");
+        }
+        [Button]
+        public void Write()
+        {
+            string[] data = new string[] { "A", "A", "A" };
+            IO.WriteJson("data/contents1.json", data);
+        }
+        [Button]
+        public void PrintPath()
+        {
+            CDebug.Log(Application.persistentDataPath);
+        }
+    }
+}
