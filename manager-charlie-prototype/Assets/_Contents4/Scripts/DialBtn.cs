be366b14a06b91e6430ef4768260b39160a38070
hanwooyoung <hanwoozz91@gmail.com>
2017-08-29 14:23:40 +0900

    삼각함수를 이용한 스와이프 구현으로 재구성, 제자리 찾아가는 것까지 구현

---
+++ manager-charlie-prototype/Assets/_Contents4/Scripts/DialBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialBtn : MonoBehaviour {
+
+    public int Index = 0;
+    public Sprite InstBody;
+    //public float Angle;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Angle += 0.1f;
+
+        //transform.position =new Vector2(3.2f * Mathf.Cos(0), 3.2f * Mathf.Sin(0)) ;
+
+	}
+}
