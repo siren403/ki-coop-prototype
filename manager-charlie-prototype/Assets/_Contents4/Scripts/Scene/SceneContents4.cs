13313e86631f0a4dea906059b92a197aa92fd6a8
hanwooyoung <hanwoozz91@gmail.com>
2017-08-30 17:57:08 +0900

    링크드리스트 이용해서 각도다이얼 단추 index 값을 변환

---
+++ manager-charlie-prototype/Assets/_Contents4/Scripts/Scene/SceneContents4.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomDebug;
+
+namespace Contents4
+{
+    public class SceneContents4 : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+        }
+
+        private void OnMouseDown()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+
+    }
+}
+
