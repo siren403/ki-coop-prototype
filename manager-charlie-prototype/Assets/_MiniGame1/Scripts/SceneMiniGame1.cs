aecd3cb0334326c52ea8c1d291d7bc2d023cf57f
Byeongyup <kby7139@gmail.com>
2017-08-31 18:58:33 +0900

    Scene 작업 및 SceneMiniGame1.cs, ViewMiniGame1.cs 추가

---
+++ manager-charlie-prototype/Assets/_MiniGame1/Scripts/SceneMiniGame1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomDebug;
+using Contents.QnA;
+using LitJson;
+using System.Linq;
+using Util;
+
+namespace MiniGame1
+{
+    public class SceneMiniGame1 : MonoBehaviour
+    {
+
+
+        void Awake()
+        {
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ manager-charlie-prototype/Assets/_MiniGame1/Scripts/ViewMiniGame1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using LitJson;
+using CustomDebug;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Contents.QnA;
+using UIComponent;
+using DG.Tweening;
+using System.Linq;
+
+namespace MiniGame1
+{
+    public class ViewMiniGame1 : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
