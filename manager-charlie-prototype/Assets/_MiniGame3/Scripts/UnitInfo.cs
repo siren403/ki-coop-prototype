c7bf534d4055238351b115da9ed8755647dbcd64
MASTER\Administrator <sarangsiteru>
2017-08-31 17:28:53 +0900

    MiniGame3 씬추가 및 구조 작업중

---
+++ manager-charlie-prototype/Assets/_MiniGame3/Scripts/UnitInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGame3
+{
+     public class UnitInfo 
+    {
+
+        public Item[] SetItems = new Item[3];
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        
+        }
+    }
+
+}
+   
