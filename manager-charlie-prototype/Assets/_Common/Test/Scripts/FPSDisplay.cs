7f5c9a9911827d8b92512e7922f9ab388302a68b
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-09-02 15:45:14 +0900

    - 디바이스 테스트 Scene 추가

---
+++ manager-charlie-prototype/Assets/_Common/Test/Scripts/FPSDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSDisplay : MonoBehaviour
+{
+    float deltaTime = 0.0f;
+    public GUIStyle style = new GUIStyle();
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+       
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        GUI.Label(rect, text, style);
+    }
+}
